fe355143ec069862dfb7a56e6d8767a37e5e2967
dkdoes <david.kennedy.does@gmail.com>
2017-01-09 06:39:59 -0800

    more fullwidth; pc blur output

--- mod.js
+++ mod.js
@@ -2,70 +2,9 @@ var input = document.getElementById("in")
 var output = document.getElementById("out")
 var copyAlert = $('#copyAlert')
 var test = document.getElementById("test")
-fullwidth = {
-    0:'\uff10',
-    1:'\uff11',
-    2:'\uff12',
-    3:'\uff13',
-    4:'\uff14',
-    5:'\uff15',
-    6:'\uff16',
-    7:'\uff17',
-    8:'\uff18',
-    9:'\uff19',
-    A:'\uff21',
-    B:'\uff22',
-    C:'\uff23',
-    D:'\uff24',
-    E:'\uff25',
-    F:'\uff26',
-    G:'\uff27',
-    H:'\uff28',
-    I:'\uff29',
-    J:'\uff2a',
-    K:'\uff2b',
-    L:'\uff2c',
-    M:'\uff2d',
-    N:'\uff2e',
-    O:'\uff2f',
-    P:'\uff30',
-    Q:'\uff31',
-    R:'\uff32',
-    S:'\uff33',
-    T:'\uff34',
-    U:'\uff35',
-    V:'\uff36',
-    W:'\uff37',
-    X:'\uff38',
-    Y:'\uff39',
-    Z:'\uff3a',
-    a:'\uff41',
-    b:'\uff42',
-    c:'\uff43',
-    d:'\uff44',
-    e:'\uff45',
-    f:'\uff46',
-    g:'\uff47',
-    h:'\uff48',
-    i:'\uff49',
-    j:'\uff4a',
-    k:'\uff4b',
-    l:'\uff4c',
-    m:'\uff4d',
-    n:'\uff4e',
-    o:'\uff4f',
-    p:'\uff50',
-    q:'\uff51',
-    r:'\uff52',
-    s:'\uff53',
-    t:'\uff54',
-    u:'\uff55',
-    v:'\uff56',
-    w:'\uff57',
-    x:'\uff58',
-    y:'\uff59',
-    z:'\uff5a'
-}
+
+fullwidth_replace = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'.split('')
+fullwidth_replace_with = '\uff01\uff02\uff03\uff04\uff05\uff06\uff07\uff08\uff09\uff0a\uff0b\uff0c\uff0d\uff0e\uff0f\uff10\uff11\uff12\uff13\uff14\uff15\uff16\uff17\uff18\uff19\uff1a\uff1b\uff1c\uff1d\uff1e\uff1f\uff20\uff21\uff22\uff23\uff24\uff25\uff26\uff27\uff28\uff29\uff2a\uff2b\uff2c\uff2d\uff2e\uff2f\uff30\uff31\uff32\uff33\uff34\uff35\uff36\uff37\uff38\uff39\uff3a\uff3b\uff3c\uff3d\uff3e\uff3f\uff40\uff41\uff42\uff43\uff44\uff45\uff46\uff47\uff48\uff49\uff4a\uff4b\uff4c\uff4d\uff4e\uff4f\uff50\uff51\uff52\uff53\uff54\uff55\uff56\uff57\uff58\uff59\uff5a\uff5b\uff5c\uff5d\uff5e'.split('')
 
 input.oninput = function(){
     var temp = input.value
@@ -74,8 +13,8 @@ input.oninput = function(){
         if(input.value[i]==' '){
             temp2+='\u3000'
         }
-        else if(fullwidth[input.value[i]]!=undefined){
-            temp2+=fullwidth[input.value[i]]+'\u3000'
+        else if (fullwidth_replace.indexOf(input.value[i])>=0){
+            temp2+=fullwidth_replace_with[fullwidth_replace.indexOf(input.value[i])]+'\u3000'
         }
         else{
             temp2+=input.value[i]+'\u3000'
@@ -94,6 +33,7 @@ output.onclick = function(){
     output.select()
     var temp = document.execCommand('copy')
     if(temp==true){
+        output.blur()
         copyAlert.show()
         setTimeout(function(){copyAlert.fadeOut()},1500)
     }
