50b888d21be9a0636ef5362d90af26d90384c36e
dkdoes <david.kennedy.does@gmail.com>
2017-01-16 01:41:49 -0800

    fixed and adjusted random

--- mod.js
+++ mod.js
@@ -5,12 +5,14 @@ var test = document.getElementById("test")
 var fullwidth_checkbox = document.getElementById("fullwidth")
 var sad_checkbox = document.getElementById("sad")
 var random_checkbox = document.getElementById("random")
+var char_swap_checkbox = document.getElementById("char_swap")
 
 fullwidth_replace = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~'.split('')
 fullwidth_replace_with = '\uff01\uff02\uff03\uff04\uff05\uff06\uff07\uff08\uff09\uff0a\uff0b\uff0c\uff0d\uff0e\uff0f\uff10\uff11\uff12\uff13\uff14\uff15\uff16\uff17\uff18\uff19\uff1a\uff1b\uff1c\uff1d\uff1e\uff1f\uff20\uff21\uff22\uff23\uff24\uff25\uff26\uff27\uff28\uff29\uff2a\uff2b\uff2c\uff2d\uff2e\uff2f\uff30\uff31\uff32\uff33\uff34\uff35\uff36\uff37\uff38\uff39\uff3a\uff3b\uff3c\uff3d\uff3e\uff3f\uff40\uff41\uff42\uff43\uff44\uff45\uff46\uff47\uff48\uff49\uff4a\uff4b\uff4c\uff4d\uff4e\uff4f\uff50\uff51\uff52\uff53\uff54\uff55\uff56\uff57\uff58\uff59\uff5a\uff5b\uff5c\uff5d\uff5e'.split('')
 
 input.oninput = function(){
     var temp = input.value
+    //char_swap_checkbox.checked && (temp = to_char_swap(temp))
     random_checkbox.checked && (temp = to_random(temp))
     fullwidth_checkbox.checked && (temp = to_fullwidth(temp))
     sad_checkbox.checked && (temp = to_sad(temp))
@@ -86,16 +88,16 @@ to_random = function(text_in){
     //using the shuffled array, we make sure just over half
     //of the words will be changed
     for(var i = 0; i<words.length;i++){
-        if(words[i].length>2&&indices.indexOf(i)<parseInt(words.length/2)+1){
+        if(words[i].length>1&&indices.indexOf(i)<parseInt(words.length/2)+1){
             var word = words[i]
             //break the word up into letters
             word = word.split('')
             console.log(word)
             
             //pick a random spot in the word/characters
-            var spot = parseInt(Math.random()*(word.length-1))+1
+            var spot = parseInt(Math.random()*(word.length))+1
             console.log(spot)
-            var interferenceChars = '_-^\/.,*+'
+            var interferenceChars = '_-^\/.,*'
             
             //pick a random interference character
             var interference = interferenceChars[parseInt(interferenceChars.length*Math.random())]
@@ -123,5 +125,15 @@ to_reverse_acrostics = function(text_in){
     return text_out
 }
 
+to_char_swap = function(text_in){
+    var text_out = ''
+    for (var i = 0; i<text_in.length; i++){
+        text_out+=text_in[i]
+    }
+    return text_out
+}
+
+random_checkbox.onchange = input.oninput
+//char_swap_checkbox.onchange = input.oninput
 fullwidth_checkbox.onchange = input.oninput
 sad_checkbox.onchange = input.oninput
